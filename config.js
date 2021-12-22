d07920716e1e61f409ad0211355a37fa6321f203
edik1045 <66965109+edik1045@users.noreply.github.com>
2021-12-22 11:01:23 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    host: 'localhost',
+    port: 9352,
+    directory: __dirname,
+    dir: __dirname,
+    season_num: 19, // Update this whenever there is a new season.
+    banner: 'https://cdn.discordapp.com/attachments/839921354110664746/922240176175067166/snowbanner.jpg',
+
+    background: 'worldcup'
+    /*
+    worldcup - WorldCup
+    winter19 - idk
+    GalileoLite - Star Wars Lite
+    halloween2020 - Halloween 
+    season15xmas - Cool XMas Background
+    fortnitemares - Somehow Winterfest 2019 
+    BlackMonday - Batman 
+    Galileo - Star Wars
+    season16 - Primal Season
+    season15 - Shit season
+    season15xmas - Winterfest 2020
+    season14 - Marvel
+    season13 - Water season
+    season12 - Spy season 
+    season11 - Chapter 2
+    seasonx - Season 10
+    */
+}
